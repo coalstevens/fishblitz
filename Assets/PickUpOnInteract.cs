bb21ee573faaf624267fd4ed0f67767df83997ce
coalstevens <62672483+coalstevens@users.noreply.github.com>
2025-02-16 22:14:56 -0700

    Added cliff sprites. Mushroom worldobjects wip

---
+++ Assets/PickUpOnInteract.cs
@@ -0,0 +1,26 @@
+using UnityEngine;
+
+public class PickUpOnInteract : MonoBehaviour, PlayerInteractionManager.IInteractable
+{
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    [SerializeField] private Inventory _playerInventory;
+    [SerializeField] private Inventory.ItemType _item;
+    [SerializeField] private int _itemQuantity;
+    [SerializeField] private AudioClip _pickUpSFX;
+    [SerializeField] private float _SFXVolume = 0.5f;
+
+    public bool CursorInteract(Vector3 cursorLocation)
+    {
+        if (_playerInventory.TryAddItem(_item.ItemName, _itemQuantity))
+        {
+            if (_pickUpSFX != null)
+                AudioManager.Instance.PlaySFX(_pickUpSFX, _SFXVolume);
+            Destroy(gameObject);
+        }
+        else
+        {
+            PlayerDialogueController.Instance.PostMessage("I don't have space for this");
+        }
+        return true;
+    }
+}
--- Assets/Scripts/Items/Kokanee.cs
+++ Assets/Scripts/Items/Kokanee.cs
@@ -5,9 +5,12 @@ public class Kokanee : Inventory.ItemType, Diet.IFood, PlayerInteractionManager.
 {
     [SerializeField] private Inventory _inventory;
     [SerializeField] private PlayerData _playerData;
-    public int Protein { get; } = 10;
-    public int Carbs { get; } = 0;
-    public int Nutrients { get; } = 0;
+    [SerializeField] private int _protein = 10;
+    [SerializeField] private int _carbs = 0;
+    [SerializeField] private int _nutrients = 0;
+    public int Protein => _protein;
+    public int Carbs => _carbs;
+    public int Nutrients => _nutrients;
     public int EnergyCost => 0;
 
     public void PlayToolHitSound()
